d1eeb44a8e04438a6477b4bf1dd1b85822d1661c
alistair0111 <alistair.baretto@gmail.com>
2021-07-17 20:32:29 +0530

    typegraphql Setup

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
-import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+// import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
-    const orm = await MikroORM.init(microConfig);
-    await orm.getMigrator().up();
-    const post = orm.em.create(Post, {title:'my first post'});
-    await orm.em.persistAndFlush(post);
-    console.log('-------------sql 2-------------');
-    await orm.em.nativeInsert(Post, {title:'my first post 2'});
+	const orm = await MikroORM.init(microConfig);
+	await orm.getMigrator().up();
 
-}
+	const app = express();
 
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver],
+			validate: false,
+		}),
+	});
 
-main().catch(err=>{
-    console.log(err);
+	apolloServer.applyMiddleware({ app });
+
+	app.listen(4000, () => {
+		console.log('Server started on localhost:4000');
+	});
+};
+
+main().catch((err) => {
+	console.log(err);
 });
